ae309dd0e677db1cc9f0c799a635a9a4fe423ec1
Alexander Pepper <alexander.pepper@infopark.de>
2016-12-08 13:48:29 +0100

    Partly executed "bundle exec rails app:update"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,14 @@
 Rails.application.configure do
   config.action_controller.perform_caching = false
+  config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
   config.assets.debug = true
   config.assets.digest = true
+  config.assets.quiet = true
   config.assets.raise_runtime_errors = true
   config.cache_classes = false
+  config.cache_store = :null_store
   config.consider_all_requests_local = true
   config.eager_load = false
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   config.action_dispatch.show_exceptions = false
   config.action_mailer.delivery_method = :test
   config.active_support.deprecation = :stderr
-  config.active_support.test_order = :random
   config.cache_classes = true
   config.consider_all_requests_local = true
   config.eager_load = false
