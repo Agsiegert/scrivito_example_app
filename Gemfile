7175bbe34066b96420846340680fa53353d22a66
Antony Siegert <antony.siegert@infopark.de>
2016-12-09 14:11:21 +0100

    Initial commit

--- Gemfile
+++ Gemfile
@@ -5,10 +5,11 @@ gem 'bootstrap-sass'
 gem 'dotenv-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
-gem 'rails'
-gem 'rails_12factor', group: :production
+gem 'rails', '5.0.0.1'
 gem 'sass-rails', '~> 5.0'
 gem 'scrivito', '1.7.0.rc1'
 gem 'scrivito_advanced_editors'
 gem 'secure_headers'
 gem 'uglifier'
+
+ruby '2.3.1'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,4 +12,9 @@ Rails.application.configure do
   config.log_level = :debug
   config.log_tags = [:uuid]
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
