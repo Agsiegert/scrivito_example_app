a91fecb2138b5af1d4e37b642e54db75a84da2f6
Antony Siegert <antony.siegert@infopark.de>
2017-04-05 18:04:16 +0200

    update specs based on feedback

--- spec/requests/blog_page_spec.rb
+++ spec/requests/blog_page_spec.rb
@@ -2,27 +2,48 @@ require 'rails_helper'
 
 RSpec.describe "BlogPage", type: :request do
   describe "GET /blog" do
-    it "renders the blog" do
-      # see https://scrivito.com/sdk-cheat-sheet
-      posts = BlogPostPage.all.order(published_at: :desc).to_a
-
+    before do
       get "/blog"
-
+      @posts = BlogPostPage.all.order(published_at: :desc).to_a
+    end
+    it "renders the blog overview page" do
       expect(response).to have_http_status(200)
 
       expect(response.body).to include "Tags"
       expect(response.body).to include "Feed"
       expect(response.body).to include "Photo Stream"
+      assert_select "a[href=?]", "/#{@posts[0].title.parameterize}-#{@posts[0].id}"
+    end
 
-      # the controller limits the page to the last 3 recent posts
-      assert_select "h4", posts[0].title
-      assert_select "h4", posts[1].title
-      assert_select "h4", posts[2].title
-      # check an element does not exist past our set limit.
-      assert_select "h4", count: 0, html: posts[3].title
-      # the photo stream includes links to the last 5 recent posts.
-      expect(response.body).to_not include posts[5].title
-      # Older only shows if there are more than 3 posts
+    it "shows titles of only the last 3 recent posts" do
+      assert_select "div.col-md-9" do
+        assert_select "div.row", 3 do
+          assert_select "h4", @posts[0].title
+          assert_select "h4", @posts[1].title
+          assert_select "h4", @posts[2].title
+        end
+      end
+    end
+
+    it "does not include titles of more than the last 3 recent posts" do
+      assert_select "h4", count: 0, html: @posts[3].title
+    end
+
+    it "includes images to the last 5 recent posts in the photo stream" do
+      assert_select "div.col-md-3" do
+        assert_select "div.panel" do
+          assert_select "h2.panel-title", "Photo Stream"
+          assert_select "ul.list-inline li a img", 5
+          assert_select "a[href=?]", "/#{@posts[0].title.parameterize}-#{@posts[0].id}"
+          assert_select "a[href=?]", "/#{@posts[1].title.parameterize}-#{@posts[1].id}"
+          assert_select "a[href=?]", "/#{@posts[2].title.parameterize}-#{@posts[2].id}"
+          assert_select "a[href=?]", "/#{@posts[3].title.parameterize}-#{@posts[3].id}"
+          assert_select "a[href=?]", "/#{@posts[4].title.parameterize}-#{@posts[4].id}"
+        end
+      end
+    end
+
+    it "has pagination" do
       assert_select ".previous", "← Older"
       expect(response.body).to_not include "Newer"
     end
